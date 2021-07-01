7bfb94cd7d1ba0af9575a30ea9694f2d005ae8d7
gularfeenansari <53817870+gularfeenansari@users.noreply.github.com>
2021-07-01 22:40:57 +0530

    Update smaller.cc

--- Day3/smaller.cc
+++ Day3/smaller.cc
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    int linsearch(vector<int> arr,int x)
+    {
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]==x)
+            {
+                return i;
+            }
+        }
+        cout<<"not found;";
+        return -1;
+    }
+    
+ 
 
+    
+    
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> result;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {            
+            result.push_back(nums[i]);
+        }
+        
+        sort(result.begin(),result.end());
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            
+            int index = Search(result,nums[i]);
+            cout<<index;
+            nums[i] = index;
+        }
+        
+        return nums;
+    }
+};
