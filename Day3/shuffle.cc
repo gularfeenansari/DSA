6c24f4da3a9d427216b1190619322f0fa27b6ec5
gularfeenansari <53817870+gularfeenansari@users.noreply.github.com>
2021-07-01 23:00:26 +0530

    Update shuffle.cc

--- Day3/shuffle.cc
+++ Day3/shuffle.cc
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int l=s.length();
+        char arr[l];
+        string ss="";
+        for(int i=0;i<indices.size();i++)
+        {
+            arr[indices[i]] = s[i];
+        }
+        
+        for(int i=0;i<indices.size();i++)
+        {
+            ss+=arr[i];
+        }
+        
+        return ss;
+        
+        
+    }
+};
