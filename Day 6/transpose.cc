a1eeab0709716ce8d7429a38ba5343c72ec792d6
gularfeenansari <53817870+gularfeenansari@users.noreply.github.com>
2021-07-05 13:24:38 +0530

    Update transpose.cc

--- Day 6/transpose.cc
+++ Day 6/transpose.cc
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows = matrix.size(),columns=matrix[0].size(),c=0;
+        vector< vector<int>> a(columns,vector<int>(rows));
+        
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<columns;j++)
+            {
+                a[j][i] = matrix[i][j];
+            }
+            
+        }
+        
+        return a;
+    }
+};
